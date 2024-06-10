f607f98fa9baead14ec6a3cf47102e61ca6aa6be
MCamiQZ <117488924+MCamiQZ@users.noreply.github.com>
2024-06-10 14:54:41 -0300

    clase 2
    Clase 2

---
+++ Clase 2 - Coder/Program.cs
@@ -0,0 +1,26 @@
+﻿int pass = 123456789;
+int passIngresado;
+String perfilingresado;
+
+Console.WriteLine("ingrese su perfil");
+perfilingresado = Console.ReadLine();
+Console.WriteLine("Ingrese su contraseña");
+passIngresado = Convert.ToInt32(Console.ReadLine());
+
+if (pass == passIngresado)
+{
+    switch (perfilingresado)
+    {
+        case "A":
+            Console.WriteLine("Bienvenido Administrador");
+            break;
+        case "U":
+             Console.WriteLine("Bienvenido usuario");
+            break;
+       
+    }
+}
+else
+{
+    Console.WriteLine("Debe reiniciar el sistema");
+}
--- Clase 2 - Coder/obj/Debug/net8.0/Clase 2 - Coder.AssemblyInfo.cs
+++ Clase 2 - Coder/obj/Debug/net8.0/Clase 2 - Coder.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Clase 1 - Coder")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Clase 2 - Coder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2deea76a198360a12fc1c6af0b528e89f9a9f83e")]
-[assembly: System.Reflection.AssemblyProductAttribute("Clase 1 - Coder")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Clase 1 - Coder")]
+[assembly: System.Reflection.AssemblyProductAttribute("Clase 2 - Coder")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Clase 2 - Coder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
